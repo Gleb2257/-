89dfff9865f03a42212425a506983ec32f64131d
Gleb2257 <165806553+Gleb2257@users.noreply.github.com>
2024-09-13 18:39:50 +0300

    Add files via upload

---
+++ Простые структуры данных задание 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+    int a[10];      
+    int n = 10;     // количество элементов массива
+    int i = 0;
+
+    // Создание генератора случайных чисел
+    srand(time(NULL));
+
+    // Заполнение массива случайными числами
+    while (i < n)
+    {
+        a[i] = rand() % 100;  // присваиваем каждому элементу случайное число от 0 до 99
+        i++;
+    }
+
+    // Вывод массива на экран
+    i = 0;
+    while (i < n)
+    {
+        printf("%d ", a[i]);
+        i++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ Простые структуры данных задание 5.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+struct student {
+    char famil[20];  // фамилия
+    char name[20];   // имя
+    char facult[20]; // факультет
+    int Nomzach;     // номер зачетной книжки
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian"); // Поддержка русского языка
+
+    student stud[3];
+    int i;
+
+    // Ввод данных о студентах
+    for (i = 0; i < 3; i++) {
+        cout << "Введите фамилию студента: ";
+        cin >> stud[i].famil;
+
+        cout << "Введите имя студента: ";
+        cin >> stud[i].name;
+
+        cout << "Введите название факультета студента: ";
+        cin >> stud[i].facult;
+
+        cout << "Введите номер зачетной книжки студента: ";
+        cin >> stud[i].Nomzach;
+    }
+
+    // Поиск студента по фамилии
+    char searchFamil[20];
+    cout << "\nВведите фамилию для поиска: ";
+    cin >> searchFamil;
+
+    bool found = false;
+    for (i = 0; i < 3; i++) {
+        if (strcmp(stud[i].famil, searchFamil) == 0) {
+            cout << "\nНайден студент: " << stud[i].famil << " " << stud[i].name << "\n";
+            cout << "Факультет: " << stud[i].facult << "\n";
+            cout << "Номер зачетной книжки: " << stud[i].Nomzach << "\n";
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "Студент с фамилией " << searchFamil << " не найден.\n";
+    }
+
+    return 0;
+}
